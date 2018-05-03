a4a9597582edd44e0136b2d80fe62adb33eb9c51
AnitaMartinez <amartinezaguilar6@gmail.com>
2018-05-03 18:07:42 +0200

    Red: no suma puntos si acierto muy lento

--- jasmine-standalone-3.1.0/spec/specApp.js
+++ jasmine-standalone-3.1.0/spec/specApp.js
@@ -8,13 +8,15 @@
 *      Si acierto pregunta entre 3 y 10 segundos (inclusive) - sumo 1 punto
 *          (1 punto, correcta, 5 segundos) -> 2 puntos
 *          (1 punto, correcta, 3 segundos) -> 2 puntos
+*      Si acierto y tardo mas de 11 segundos (inclusive) - 0 puntos
+           (1 punto, correcta, 11 segundos)  -> 1 punto
+           (5 punto, correcta, 18 segundos)  -> 5 punto
 *      Si fallo pregunta en mas de 11 segundos (inclusive) - resto 2 puntos
            (2 puntos, incorrecta, 11 segundos) -> 0 puntos
            (0 puntos, incorrecta, 18 segundos) -> -2 puntos
 *      Si fallo antes de 10 segundos (inclusive) - resto 1 punto
 *          (1 punto, incorrecta, 3 segundos)  -> 0 puntos
            (10 punto, incorrecta, 10 segundos)  -> 9 puntos
-*      Si acierto y tardo mas de 10 segundos - 0 puntos
 *      No se puede pasar sin responder
 *      Si en 20 segundos no has respondido , pasa a siguiente pregunta y pierdes 3 punto
 *
@@ -25,7 +27,7 @@ describe('calculo de marcador', function () {
     function recalcularMarcador(puntos, esCorrecta, tiempo) {
         if (esCorrecta && (tiempo <= 2)) {
             return puntos + 2;
-        } else if (esCorrecta && (tiempo <= 11)) {
+        } else if (esCorrecta && (tiempo <= 10)) {
             return puntos + 1;
         }
         if (!esCorrecta && (tiempo > 10)) {
@@ -42,6 +44,10 @@ describe('calculo de marcador', function () {
     it("suma punto si acierta rápido", function () {
         expect(recalcularMarcador(1, true, 5)).toBe(2);
         expect(recalcularMarcador(1, true, 3)).toBe(2);
+    });
+    it("no suma puntos si acierto muy lento", function () {
+        expect(recalcularMarcador(1, true, 11)).toBe(1);
+        expect(recalcularMarcador(5, true, 18)).toBe(5);
     });
     it("resta más puntos si fallo muy lento", function () {
         expect(recalcularMarcador(2, false, 11)).toBe(0);
