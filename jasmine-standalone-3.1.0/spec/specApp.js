ced3a35114ae706f6f8e4bcff6db3b00c3eeab66
AnitaMartinez <amartinezaguilar6@gmail.com>
2018-05-03 17:59:40 +0200

    Red: resta punto si fallo lento

--- jasmine-standalone-3.1.0/spec/specApp.js
+++ jasmine-standalone-3.1.0/spec/specApp.js
@@ -2,16 +2,19 @@
 *
 *   velocidad de respuesta, acierto o fallo,
 *
-*      Si acierto pregunta en menos de 2 segundos - sumo 2 puntos
+*      Si acierto pregunta en menos de 2 segundos (inclusive) - sumo 2 puntos
 *          (0 puntos, pregunta correcta, 1 segundo) -> 2 puntos
 *          (1 punto, correcta, 1 segundo) -> 3 puntos
-*      Si acierto pregunta entre 2 y 10 segundos - sumo 1 punto
+*      Si acierto pregunta entre 3 y 10 segundos (inclusive) - sumo 1 punto
 *          (1 punto, correcta, 5 segundos) -> 2 puntos
-*      Si fallo pregunta en mas de 10 segundos - resto 2 puntos
-           (2 puntos, incorrecta, 11 segundos) -> 0
-           (0 puntos, incorrecta, 11 segundos) -> -2
+*          (1 punto, correcta, 3 segundos) -> 2 puntos
+*      Si fallo pregunta en mas de 11 segundos (inclusive) - resto 2 puntos
+           (2 puntos, incorrecta, 11 segundos) -> 0 puntos
+           (0 puntos, incorrecta, 18 segundos) -> -2 puntos
+*      Si fallo antes de 10 segundos (inclusive) - resto 1 punto
+*          (1 punto, incorrecta, 3 segundos)  -> 0 puntos
+           (10 punto, incorrecta, 10 segundos)  -> 9 puntos
 *      Si acierto y tardo mas de 10 segundos - 0 puntos
-*      Si fallo antes de 10 segundos - resto 1 punto
 *      No se puede pasar sin responder
 *      Si en 20 segundos no has respondido , pasa a siguiente pregunta y pierdes 3 punto
 *
@@ -22,7 +25,7 @@ describe('calculo de marcador', function () {
     function recalcularMarcador(puntos, esCorrecta, tiempo) {
         if (esCorrecta && (tiempo <= 2)) {
             return puntos + 2;
-        } else if (esCorrecta && (tiempo <= 10)) {
+        } else if (esCorrecta && (tiempo <= 11)) {
             return puntos + 1;
         }
         if (!esCorrecta && (tiempo > 10)) {
@@ -36,9 +39,14 @@ describe('calculo de marcador', function () {
     });
     it("suma punto si acierta rápido", function () {
         expect(recalcularMarcador(1, true, 5)).toBe(2);
-
+        expect(recalcularMarcador(1, true, 3)).toBe(2);
     });
     it("resta más puntos si fallo muy lento", function () {
         expect(recalcularMarcador(2, false, 11)).toBe(0);
+        expect(recalcularMarcador(0, false, 18)).toBe(-2);
+    });
+    it("resta punto si fallo lento", function () {
+        expect(recalcularMarcador(1, false, 3)).toBe(0);
+        expect(recalcularMarcador(10, false, 10)).toBe(9);
     });
 });
