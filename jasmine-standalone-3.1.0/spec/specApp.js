43471c9e9add21c0e16df5b8fbf23c07a63478a1
AnitaMartinez <amartinezaguilar6@gmail.com>
2018-05-03 17:35:38 +0200

    Red: suma punto si acierta rápido

--- jasmine-standalone-3.1.0/spec/specApp.js
+++ jasmine-standalone-3.1.0/spec/specApp.js
@@ -5,9 +5,9 @@
 *      Si acierto pregunta en menos de 2 segundos - sumo 2 puntos
 *          (0 puntos, pregunta correcta, 1 segundo) -> 2 puntos
 *          (1 punto, correcta, 1 segundo) -> 3 puntos
-*      Si fallo pregunta en mas de 10 segundos - resto 2 puntos
 *      Si acierto pregunta entre 2 y 10 segundos - sumo 1 punto
-            (1 punto, correcta, 5 segundos) -> 2 puntos
+*          (1 punto, correcta, 5 segundos) -> 2 puntos
+*      Si fallo pregunta en mas de 10 segundos - resto 2 puntos
 *      Si acierto y tardo mas de 10 segundos - 0 puntos
 *      Si fallo antes de 10 segundos - resto 1 punto
 *      No se puede pasar sin responder
@@ -18,13 +18,16 @@
 
 describe('calculo de marcador', function () {
     function recalcularMarcador(puntos, esCorrecta, tiempo) {
-        if (esCorrecta && tiempo <= 2) {
+        if (esCorrecta && (tiempo <= 2)) {
             return puntos + 2;
         }
     }
 
-    it("suma mas puntos si acierta muy rapido", function () {
+    it("suma más puntos si acierta muy rápido", function () {
         expect(recalcularMarcador(0, true, 1)).toBe(2);
         expect(recalcularMarcador(2, true, 1)).toBe(4);
+    });
+    it("suma punto si acierta rápido", function () {
+        expect(recalcularMarcador(1, true, 5)).toBe(2);
     });
 });
