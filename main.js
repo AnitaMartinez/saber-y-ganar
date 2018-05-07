068ef65a0cf633678ad56c9518757aa2ed9a2e28
AnitaMartinez <amartinezaguilar6@gmail.com>
2018-05-07 15:56:46 +0200

    Paint questions in DOM

--- main.js
+++ main.js
@@ -4,41 +4,63 @@
 function getQuestions(callback) {
     var serverData = [
         {
-            question: { id: 1, text: 'Pregunta1' },
+            id: 1,
+            question: "¿Cuál es la capital de Portugal?",
             answers: [
-                { id: 1, text: 'A1' },
-                { id: 2, text: 'A2' },
-                { id: 3, text: 'A3' }
-            ],
-            correctAnswerId: 1
+                { id: 1, answerDescritpion: "Faro", isCorrect: false, idQuestion: 1 },
+                { id: 2, answerDescritpion: "Oporto", isCorrect: false, idQuestion: 1 },
+                { id: 3, answerDescritpion: "Lisboa", isCorrect: true, idQuestion: 1 }
+            ]
         },
         {
-            question: { id: 2, text: 'Pregunta2' },
+            id: 2,
+            question: "¿Cuál es la capital de Egipto?",
             answers: [
-                { id: 4, text: 'B1' },
-                { id: 5, text: 'B2' },
-                { id: 6, text: 'B3' }
-            ],
-            correctAnswerId: 5
+                { id: 1, answerDescritpion: "Faro", isCorrect: false, idQuestion: 2 },
+                { id: 2, answerDescritpion: "El Cairo", isCorrect: true, idQuestion: 2 },
+                { id: 3, answerDescritpion: "Lisboa", isCorrect: false, idQuestion: 2 }
+            ]
         },
         {
-            question: { id: 3, text: 'Pregunta3' },
+            id: 3,
+            question: "¿Cuál es la capital de España?",
             answers: [
-                { id: 7, text: 'C1' },
-                { id: 8, text: 'C2' },
-                { id: 9, text: 'C3' }
-            ],
-            correctAnswerId: 9
-        },
-    ]
+                { id: 1, answerDescritpion: "Madrid", isCorrect: true, idQuestion: 3 },
+                { id: 2, answerDescritpion: "Oporto", isCorrect: false, idQuestion: 3 },
+                { id: 3, answerDescritpion: "Lisboa", isCorrect: false, idQuestion: 3 }
+            ]
+        }
+    ];
     callback(serverData);
-};
+}
 
+const paintQuestions = questionsWithAnswers => {
+    const questionsList = document.getElementById("questions-list");
+    const buttonSendQuestion = document.getElementById("button-submit");
+
+    var i = 0;
+    function paintQuestions() {
+        if (i < questionsWithAnswers.length) {
+            questionsList.innerHTML = `<h5>${questionsWithAnswers[i].question}</h5>`
+
+            for (const answers of questionsWithAnswers[i].answers) {
+                questionsList.innerHTML += (
+                    `<input type="radio" id=${answers.answerDescritpion} name="answer" value="answer">
+                    <label for=${answers.answerDescritpion}>${answers.answerDescritpion}</label>`
+                );
+            }
+            i++;
+        }
+    }
+    buttonSendQuestion.addEventListener("click", paintQuestions);
+}
 
 //Get the data
-var questions = [];
+var questionsWithAnswers = [];
 getQuestions(function (data) {
-    questions = data;
-    console.log(questions);
-
+    questionsWithAnswers = data;
+    console.log(questionsWithAnswers);
+    paintQuestions(questionsWithAnswers);
 });
+
+
