d22d5568bd858fc95d32052167637e15a069c653
“Ana” <amartinezaguilar6@gmail.com>
2018-05-30 16:53:35 +0200

    Fake a server to serve the questions

---
+++ src/game.js
@@ -0,0 +1,324 @@
+export default function createGame(client){
+  let questionsWithAnswers = [];
+  let currentQuestionIndex = 0;
+  let answerUserId;
+  let currentIdQuestion;
+  const answersUser = [];
+  let timeCounter;
+  const accumulatorTimeCounter = {
+      accumulator: 0
+  };
+  const maximumTimeCounter = 5;
+
+  function init() {
+      const buttonStartGame = document.getElementById("button-init-questions");
+      const buttonSendQuestion = document.getElementById("button-send-question");
+      buttonStartGame.addEventListener("click", onStart);
+      buttonSendQuestion.addEventListener("click", onNextQuestion);
+      client.getQuestions(function (questions) {
+        return questionsWithAnswers = questions;
+      })
+  }
+
+  function getQuestions(callback) {
+      var serverData = [
+          {
+              id: 0,
+              questionText: "¿Cuál es la capital de Portugal?",
+              answers: [
+                  { id: 1, answerText: "Faro", idQuestion: 0 },
+                  { id: 2, answerText: "Oporto", idQuestion: 0 },
+                  { id: 3, answerText: "Lisboa", idQuestion: 0 }
+              ],
+              correctAnswerId: 3
+          },
+          {
+              id: 1,
+              questionText: "¿Cuál es la capital de Egipto?",
+              answers: [
+                  { id: 1, answerText: "Faro", idQuestion: 1 },
+                  { id: 2, answerText: "El Cairo", idQuestion: 1 },
+                  { id: 3, answerText: "Lisboa", idQuestion: 1 }
+              ],
+              correctAnswerId: 2
+          },
+          {
+              id: 2,
+              questionText: "¿Cuál es la capital de España?",
+              answers: [
+                  { id: 1, answerText: "Madrid", idQuestion: 2 },
+                  { id: 2, answerText: "Oporto", idQuestion: 2 },
+                  { id: 3, answerText: "Lisboa", idQuestion: 2 }
+              ],
+              correctAnswerId: 1
+          },
+          {
+              id: 3,
+              questionText: "¿Cuál es la capital de Francia?",
+              answers: [
+                  { id: 1, answerText: "Madrid", idQuestion: 3 },
+                  { id: 2, answerText: "París", idQuestion: 3 },
+                  { id: 3, answerText: "Lisboa", idQuestion: 3 }
+              ],
+              correctAnswerId: 2
+          }
+      ];
+      callback(serverData);
+  }
+
+  function onStart() {
+      resetQuestionsAndAnswers();
+      showTimer();
+      showButtonNextQuestion();
+      paintQuestion(currentQuestion());
+      updateQuestionIndex();
+      startCountdown();
+      prepareAnswersToBeClicked();
+      updateScoreboard();
+  }
+
+  function onNextQuestion() {
+      if (!isAnyAnswerChecked() && !isTimeOut()) {
+          forceUserToAnswer();
+          return;  //claúsula guarda
+      }
+
+      if (!isAnyAnswerChecked() && isTimeOut()) {
+          markAnswerAsNotAnswered();
+      }
+      if (areThereMoreQuestions()) {
+          saveInfoAnswerUser();
+          prepareStepsToPlayQuestion();
+      }
+      else {
+          saveInfoAnswerUser();
+          resetGame();
+          init();
+      }
+      updateScoreboard();
+  }
+
+  function prepareStepsToPlayQuestion() {
+      clearCountDown();
+      paintQuestion(currentQuestion());
+      updateQuestionIndex();
+      startCountdown();
+      prepareAnswersToBeClicked();
+  }
+
+  function startCountdown() { //callback ¿?
+      timeCounter = setInterval(function () {
+          onTimeChanged();
+          onTimeOut();
+      }, 1000);
+  }
+
+  function onTimeChanged() {
+      showTime();
+      accumulatorTimeCounter.accumulator++;
+  }
+
+  function onTimeOut() {
+      if (isTimeOut()) {
+          onNextQuestion();
+      }
+  }
+
+  function isTimeOut() {
+      return accumulatorTimeCounter.accumulator > maximumTimeCounter;
+  }
+
+  function clearCountDown() {
+      clearInterval(timeCounter);
+      resetAccumulatorCountdown();
+      showTime();
+  }
+
+  function resetAccumulatorCountdown() {
+      accumulatorTimeCounter.accumulator = 0;
+  }
+
+  function showTime() {
+      const timerDom = document.getElementById("timer");
+      timerDom.innerHTML = accumulatorTimeCounter.accumulator;
+  }
+
+  function paintQuestion(question) {
+      const questionsList = document.getElementById("questions-list");
+      let titleQuestion;
+      let answersInputs = "";
+      titleQuestion = `<h5> ${question.questionText} </h5>`;
+      for (const answer of question.answers) {
+          answersInputs += (
+              `<div>
+                  <input class="input-radio" type="radio" data-idquestion=${question.id} value=${answer.id} id=${answer.answerText} name="answers">
+                  <label for=${answer.answerText}> ${answer.answerText} </label>
+              </div>`
+          );
+      }
+      questionsList.innerHTML = titleQuestion + "<div class='container-answers'>" + answersInputs + "</div>";
+      answersInputs = "";
+  }
+
+  function updateQuestionIndex() {
+      ++currentQuestionIndex;
+  }
+
+  function currentQuestion() {
+      return questionsWithAnswers[currentQuestionIndex];
+  }
+
+  function saveInfoAnswerUser() {
+      answersUser.push({
+          idQuestion: currentIdQuestion,
+          idAnswer: answerUserId,
+          isCorrect: isAnswerCorrect(),
+          time: accumulatorTimeCounter.accumulator
+      });
+      console.log(answersUser);
+  }
+
+  function showButtonNextQuestion() {
+      const buttonSendQuestion = document.getElementById("button-send-question");
+      buttonSendQuestion.classList.remove('hidden');
+  }
+
+  function showTimer() {
+      const timerDom = document.getElementById("timer");
+      timerDom.classList.remove('hidden');
+  }
+
+  function areThereMoreQuestions() {
+      return currentQuestionIndex < questionsWithAnswers.length;
+  }
+
+  function isAnyAnswerChecked() {
+      const inputsRadio = document.getElementsByClassName('input-radio');
+      for (let i = 0; i < inputsRadio.length; i++) {
+          if (inputsRadio[i].checked === true) {
+              return true;
+          }
+      }
+      return false;
+  }
+
+  function markAnswerAsNotAnswered() {
+      answerUserId = null;
+  }
+
+  function forceUserToAnswer() {
+      alert("Elige una respuesta antes de pasar a la siguiente");
+  }
+
+  function prepareAnswersToBeClicked() {
+      const inputsRadio = document.getElementsByClassName('input-radio');
+      for (let i = 0; i < inputsRadio.length; i++) {
+          inputsRadio[i].addEventListener("click", getInfoAnswerUser);
+      }
+  }
+
+  function getInfoAnswerUser(event) {
+      currentIdQuestion = parseInt(event.currentTarget.dataset.idquestion);
+      answerUserId = parseInt(event.currentTarget.value);
+  }
+
+  function isAnswerCorrect() {
+      if (answerUserId === null) {
+          console.log("Has fallado, puesto que no has contestado y se ha pasado el tiempo");
+          return false;
+      }
+      else if (answerUserId === questionsWithAnswers[currentIdQuestion].correctAnswerId) {
+          console.log("Has acertado");
+          return true;
+      } else {
+          console.log("Has fallado");
+          return false;
+      }
+  }
+
+  function updateScoreboard() {
+      paintTotalPoints(calculatePoints());
+      paintCorrectAnswers();
+      paintTotalAnswers();
+  }
+
+  function paintTotalPoints(points) {
+      const txtTotalPoints = document.getElementById("total-points");
+      txtTotalPoints.innerText = points;
+  }
+
+  function paintTotalAnswers() {
+      const txtTotalAnswers = document.getElementById("total-answers");
+      txtTotalAnswers.innerText = answersUser.length;
+  }
+
+  function paintCorrectAnswers() {
+      const text = document.getElementById("correct-answers");
+      text.innerText = calculateNumberOfCorrectAnswers();
+  }
+
+  function calculatePoints() {
+      let points = 0;
+      for (let i = 0; i < answersUser.length; i++) {
+          if (answersUser[i].isCorrect) {
+              if (answersUser[i].time > 10) {
+                  console.log("Correcta, más de 10 segundos");
+                  points += 1;
+              }
+              else if (answersUser[i].time < 3) {
+                  console.log("Correcta, en menos de 3 segundos");
+                  points += 3;
+              }
+              else {
+                  console.log("Correcta, entre 3 y 10 segundos");
+                  points += 2;
+              }
+          }
+          if (answersUser[i].isCorrect === false) {
+              if (answersUser[i].time > maximumTimeCounter) {
+                  points -= 3;
+              }
+              else if (answersUser[i].time > 10) {
+                  points -= 2;
+              }
+              else {
+                  points -= 1;
+              }
+          }
+      }
+      return points;
+  }
+
+  function calculateNumberOfCorrectAnswers() {
+      let accumulatorAnswers = 0;
+      for (let i = 0; i < answersUser.length; i++) {
+          if (answersUser[i].isCorrect === true) {
+              accumulatorAnswers += 1;
+          }
+      }
+      return accumulatorAnswers;
+  }
+
+  function resetQuestionsAndAnswers() {
+      currentQuestionIndex = 0;
+      answersUser.length = 0;
+  }
+
+  function resetGame() {
+      clearCountDown();
+      resetDOM();
+  }
+
+  function resetDOM() {
+      const questionsList = document.getElementById("questions-list");
+      questionsList.innerHTML = "<button id='button-init-questions'>Volver a jugar</button>";
+      const timerDom = document.getElementById("timer");
+      timerDom.classList.add('hidden');
+      const buttonSendQuestion = document.getElementById("button-send-question");
+      buttonSendQuestion.classList.add('hidden');
+  }
+
+  return {
+      start: init
+  };
+}
--- src/server.js
+++ src/server.js
@@ -11,5 +11,52 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/api/questions', (request, response) => {
+    let questions = [
+      {
+          id: 0,
+          questionText: "¿Cuál es la capital de Portugal?",
+          answers: [
+              { id: 1, answerText: "Faro", idQuestion: 0 },
+              { id: 2, answerText: "Oporto", idQuestion: 0 },
+              { id: 3, answerText: "Lisboa", idQuestion: 0 }
+          ],
+          correctAnswerId: 3
+      },
+      {
+          id: 1,
+          questionText: "¿Cuál es la capital de Egipto?",
+          answers: [
+              { id: 1, answerText: "Faro", idQuestion: 1 },
+              { id: 2, answerText: "El Cairo", idQuestion: 1 },
+              { id: 3, answerText: "Lisboa", idQuestion: 1 }
+          ],
+          correctAnswerId: 2
+      },
+      {
+          id: 2,
+          questionText: "¿Cuál es la capital de España?",
+          answers: [
+              { id: 1, answerText: "Madrid", idQuestion: 2 },
+              { id: 2, answerText: "Oporto", idQuestion: 2 },
+              { id: 3, answerText: "Lisboa", idQuestion: 2 }
+          ],
+          correctAnswerId: 1
+      },
+      {
+          id: 3,
+          questionText: "¿Cuál es la capital de Francia?",
+          answers: [
+              { id: 1, answerText: "Madrid", idQuestion: 3 },
+              { id: 2, answerText: "París", idQuestion: 3 },
+              { id: 3, answerText: "Lisboa", idQuestion: 3 }
+          ],
+          correctAnswerId: 2
+      }
+    ];
+    response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify(questions));
+});
+
 // START APP
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
